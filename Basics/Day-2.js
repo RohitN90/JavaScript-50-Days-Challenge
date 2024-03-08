eafc231a0b36aeeb2056773fbbb64a2b48d801be
Rohit vardhan <121122563+RohitN90@users.noreply.github.com>
2024-03-08 16:32:25 +0000

    Learn about const,let and var,commits.

---
+++ Basics/Day-2.js
@@ -0,0 +1,29 @@
+// Learning variables and const, let and var,Commits.
+
+//Ths const keyword help the variable to have a constant value in all time.
+const accoutId = 540;
+
+//These remaining keywords can be changed or modify the values of variables.
+let accoutEmail = "rohit90@gmail.com";
+
+/*
+Don't use the var keyword in the code because the var keyword have a proper scope issue. 
+*/
+var accoutPassword = "123456";
+accoutCity = "vishakhapatnam";
+
+//Eample
+accoutEmail = "Teena@hotmail.com";
+accoutPassword = "3939";
+accoutCity = "USA";
+//we can't change it like this --> Its show's Error
+// accoutId = 23;
+
+//This is a normal approch of printing the varivables
+console.log(accoutId);
+
+//This is new method of printing n no.of.variables in a [] or array.
+console.table([accoutEmail, accoutPassword, accoutCity]);
+
+
+// <<------------------------------------Bye this Day-2 is conplected------------------------------------------>>
