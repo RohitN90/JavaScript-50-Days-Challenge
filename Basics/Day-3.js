5fc1994bcaf020aed33238699efa48bfd6dd6f34
Rohit vardhan <121122563+RohitN90@users.noreply.github.com>
2024-03-09 17:48:29 +0000

    Learned DataTypes and ECMA scripts standes

---
+++ Basics/Day-3.js
@@ -0,0 +1,18 @@
+//Today I am learning About Data-Types and ECMA 
+
+//tc39 and mdn are the website which are usefull for learning the javascript and ECMA script.
+
+//In javascript there are few datatypes but do all the works. These are the few :
+
+let name = "Rohit"; // String dataType.
+let age = 23; // Int or integer dataType.
+let active = true; // Boolean dataType.
+let h = null; // null is a stand alone value or its defines a variable as "EMPTY".
+let state; // undefined is a type where you don't define any value to a variable in js.
+let m = Symbol; // symbol is used to define uniquness 
+
+// The above datatypes are know as premitive dataTypes.
+
+// The typeOf is used to define what is the type of a values or a variable.
+console.log(typeof "Rohit");
+console.log(typeof age);
